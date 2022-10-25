1f58aa4c9ea2d829ba2ca17568c2a87abef6e052
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2022-10-25 11:46:20 -0300

    Projeto escola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'escola-root',
-  templateUrl: './app.component.html',
+  selector: 'escola-root', //onde vou injetar esse componente
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'escola';
+  title = 'Escola Hunter';
+  name = 'Yuri';
+  nota = 7;
+  situacao = 'Aprovado';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { AlunoComponent } from './aluno/aluno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule
